ad0b923a55651ebd883d1472669007ff8e17e72f
TheJohny71 <jduval71@me.com>
2024-12-03 17:05:47 -0600

    Delete design-system/components/Navigation directory

--- design-system/components/Navigation/index.js
+++
@@ -1,7 +0,0 @@
-export { default as GlobalNavigation } from './GlobalNavigation';
-export { default as NotificationCenter } from './NotificationCenter';
-export { default as UserPreferences } from './UserPreferences';
-
-// Export types if using TypeScript
-export type { NotificationType } from './NotificationCenter';
-export type { PreferencesType } from './UserPreferences';
