6882949283fb04db3273c0464c2c072d890d0d13
TheJohny71 <jduval71@me.com>
2024-12-03 10:03:03 -0600

    Create index.js

---
+++ design-system/components/Navigation/index.js
@@ -0,0 +1,7 @@
+export { default as GlobalNavigation } from './GlobalNavigation';
+export { default as NotificationCenter } from './NotificationCenter';
+export { default as UserPreferences } from './UserPreferences';
+
+// Export types if using TypeScript
+export type { NotificationType } from './NotificationCenter';
+export type { PreferencesType } from './UserPreferences';
