908e7a51580fddad29b9e0339e9a519a47676317
TheJohny71 <jduval71@me.com>
2024-12-03 21:56:35 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import GlobalNavigation from './components/Navigation'
 import LeaveManagement from './components/LeaveManagement'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <LeaveManagement />
+    <div className="min-h-screen bg-[#F5F5F7]">
+      <GlobalNavigation />
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <LeaveManagement />
+      </main>
     </div>
   )
 }
