5bc06e58f5a9a028554550363d7ae490919ed961
TheJohny71 <jduval71@me.com>
2024-12-03 18:51:29 -0600

    Update and rename app.jsx to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LeaveManagement from './components/LeaveManagement'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <LeaveManagement />
+    </div>
+  )
+}
+
+export default App
